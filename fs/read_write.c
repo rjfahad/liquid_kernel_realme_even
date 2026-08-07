ca82e0211c2e57dd6f429df6fcf528c38326bf40
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-08-07 07:08:53 +0600

    ksu: add manual hooks, path_umount backport and selinux compat
    - Hook execveat/faccessat/stat/read/reboot/input for KernelSU-Next
      manual hook mode (CONFIG_KSU_MANUAL_HOOK=y)
    - Backport can_umount/path_umount into fs/namespace.c + fs/internal.h
    - Patch struct seccomp with filter_count (KSU-Next Kbuild backport)
    - Add selinux_inode/selinux_cred helpers required by KSU-Next selinux

--- fs/read_write.c
+++ fs/read_write.c
@@ -584,10 +584,13 @@ static inline void file_pos_write(struct file *file, loff_t pos)
 		file->f_pos = pos;
 }
 
+extern void ksu_handle_sys_read(unsigned int fd);
+
 SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 {
 	struct fd f = fdget_pos(fd);
 	ssize_t ret = -EBADF;
+	ksu_handle_sys_read(fd);
 #if defined(OPLUS_FEATURE_IOMONITOR) && defined(CONFIG_IOMONITOR)
 	unsigned long read_time = jiffies;
 #endif /*OPLUS_FEATURE_IOMONITOR*/
